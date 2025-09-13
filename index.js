9add12f054bc8fb9a619969fbcae14f87a6bda31
gabkun <95224438+gabkun@users.noreply.github.com>
2025-09-13 20:57:17 +0800

    abcd

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"
+import dotenv from "dotenv"
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('gwapo raph');
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
