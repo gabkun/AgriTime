021740f4b48ccf32514a1f1d326aa4f83ab76d46
gabkun <95224438+gabkun@users.noreply.github.com>
2025-09-13 23:01:44 +0800

    change filebase

--- views/index.php
+++ views/index.php
@@ -1,3 +1,9 @@
+<?php
+// Set timezone to Philippine time
+date_default_timezone_set('Asia/Manila');
+$currentTime = date('g:ia'); // Example: 6:25pm
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,18 +11,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Face detection on the browser using javascript !</title>
+    <title>Face Detection on the Browser using JavaScript</title>
+
+    <!-- Scripts -->
     <script defer src="face-api.min.js"></script>
     <script defer src="script.js"></script>
+
+    <!-- Styles -->
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div>
-        <title>Attendance System</title>
-        <h1>Time In:6:25pm-PHT</h1>
+        <h1>Time In: <?php echo $currentTime; ?> - PHT</h1>
     </div>
-    <video id="video" width="600" height="450" autoplay>
+
+    <video id="video" width="600" height="450" autoplay muted></video>
 </body>
 
 </html>
